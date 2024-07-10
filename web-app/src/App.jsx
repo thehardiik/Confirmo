bd89cd000b391a8be05ac1f29f11fa97d305664b
Hardik Srivastav <hardiknjms21@gmail.com>
2024-07-10 09:45:04 +0530

    Web app project setup

---
+++ web-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full h-[100vh] bg-gray-700'></div>
+    </>
+  )
+}
+
+export default App
