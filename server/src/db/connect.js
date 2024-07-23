3783609b279e28ff22ba4158e03d9a72868cc45b
Hardik Srivastav <111513563+thehardiik@users.noreply.github.com>
2024-07-23 08:29:55 +0530

    Update connect.js

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connectDB() {
     try {
-        return mongoose.connect("mongodb+srv://hardiknjms21:nomorepassword@cluster0.q6rfeva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        return mongoose.connect("")
         
     } catch (error) {
         throw error
